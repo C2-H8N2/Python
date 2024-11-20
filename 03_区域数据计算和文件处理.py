81c33bebd12df0cc2f492b98fdd45a43fbeeeeb9
C2-H8N2 <sbylcwds@gmail.com>
2024-11-20 20:49:34 +0800

    2024-11-20

--- 03_区域数据计算和文件处理.py
+++ 03_区域数据计算和文件处理.py
@@ -48,7 +48,7 @@ nc_data = nc.Dataset(r"D:\data\dataset\climate data\cru_ts4.05.1901.2020.tmp.dat
 nc_data
 #----------------------------------------------------------------------
 # %%
-#提取变量
+#提取变量为numPy数组
 #variables(dimensions): float32 lon(lon), float32 lat(lat), float32 time(time), float32 tmp(time, lat, lon), int32 stn(time, lat, lon)
 #nc_data的variables属性下各数据
 raw_lat_data = np.array(nc_data.variables['lat'])#纬度
@@ -58,43 +58,48 @@ raw_tmp_data = np.array(nc_data.variables['tmp'])#温度
 raw_tmp_data
 #----------------------------------------------------------------------
 #%%
-#提取温度缺失值，并换为nan
+#提取温度缺失值属性，并换为nan
 tmp_missing_value = nc_data.variables['tmp'].missing_value#提取missing——value属性
 raw_tmp_data[raw_tmp_data==tmp_missing_value] = np.nan 
 #---------------------------------------------------------------------- 
 ##处理时间
 # %%
-
+#将nc文件时间格式从numpy.float转换为datetime
 def cftime2datetime(cftime,units,format='%Y-%m-%d %H:%M:%S'):
-    #将nc文件时间格式从cftime转换为date
     return datetime.datetime.strptime(num2date(times=cftime, units=units).strftime(format), format)
+    #先通过num2date()将数据转为date格式，再通过.strftime()方法转为限定格式的字符串
+    # 最后datetime.datetime.strptime()将字符串转为datetime.datetime格式
 
+    #通过pd.Series()j将生成的列表转为一个一维数据结构
 clean_time_data = pd.Series([cftime2datetime(i, units='days since 1900-1-1') for i in raw_time_data])
 clean_time_data[:4]
-
+#----------------------------------------------------------------------
 ##处理边界数据并改变坐标系
 # %%
-#导入中国边界数据
-china_boundary = gpd.read_file(filename="D:\data\dataset\中国地图边界202111版.json")
-china_boundary_valid = china_boundary.copy()#建立副本防治改变元数据
-china_boundary_valid['geometry'] = china_boundary.buffer(0)
+#导入中国边界数据,并转为GeoDataFrame形式
+china_boundary = gpd.read_file(filename=r"D:\data\dataset\中国地图边界202111版.json")
+china_boundary_valid = china_boundary.copy()#建立副本防止改原数据
+china_boundary_valid['geometry'] = china_boundary.buffer(0)#修复无效的几何对象（如自交、重复点等）
 china_boundary_valid
-boundary=china_boundary_valid.total_bounds
-min_x,min_y,max_x,max_y=boundary
+#boundary=china_boundary_valid.total_bounds
+#min_x,min_y,max_x,max_y=boundary
+#----------------------------------------------------------------------
 #%%
 #计算边界中心点
 center_china_point = china_boundary_valid.centroid[0]
 center_china_point.x,center_china_point.y
+#----------------------------------------------------------------------
 # %%
 #提取原投影
 raw_crs = china_boundary_valid.crs
-
+raw_crs
+#----------------------------------------------------------------------
 # %%
 #设置新投影
 new_crs = ccrs.LambertConformal(central_longitude=center_china_point.x,
                                 central_latitude=center_china_point.y
                                 )
-
+#----------------------------------------------------------------------
 ##裁剪
 # %%
 #计算中国地图掩膜
@@ -102,7 +107,8 @@ def pic(lon,lat)->bool:
     #监测点是否在中国边界线内
     return china_boundary_valid.contains(Point(lon,lat))[0]
     #eg:返回值为False
-#raw_tmp_data.shape返回为（1440,360,720）
+
+#raw_tmp_data.shape返回为（1440,360,720）,根据每层的行列数生成一个值为False的二维数组
 mask_for_china = np.full(shape=raw_tmp_data.shape[1:],fill_value=False)
 #raw_lat_data.shape返回为(720,)
 for index_lat in tqdm(range(raw_lat_data.shape[0])):
@@ -110,12 +116,12 @@ for index_lat in tqdm(range(raw_lat_data.shape[0])):
         point = (raw_lon_data[index_lon],raw_lat_data[index_lat])
         mask_for_china[index_lat,index_lon]=pic(point[0],point[1])
 mask_for_china
+#----------------------------------------------------------------------
 # %%
 #将要写入tiff文件的数据，不在中国范围内的转为nan
-
 china_data= raw_tmp_data[0,:,:].copy()
 china_data[~mask_for_china] = np.nan
-
+#----------------------------------------------------------------------
 # %%
 #将矩阵保存在tiff文件中
 
@@ -123,12 +129,12 @@ def array2gtiff(array,filename):
     with rasterio.open(filename,'w',driver='GTiff',
                    height=array.shape[0],
                    width=array.shape[1],
-                   count=1,
+                   count=1,#一个波段
                    dtype=str(array.dtype)) as f:
         f.write(array,1)
-array2gtiff(array=china_data.astype('float64')[::-1,],
+array2gtiff(array=china_data.astype('float64')[::-1,],#将行颠倒
             filename=r"D:\data\dataset\result\test.tiff")
-
+#----------------------------------------------------------------------
 # %% 
 need1 = pd.DataFrame()
 need1['date'] = clean_time_data
