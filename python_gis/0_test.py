4422d7ad0237641e42cfc791d46368132ed4a7d6
C2-H8N2 <sbylcwds@gmail.com>
2024-12-17 22:23:17 +0800

    2024-12-17

--- python_gis/0_test.py
+++ python_gis/0_test.py
@@ -1,9 +1,5 @@
 #%%
 import arcpy
-
-
-# 初始化许可
-if not arcpy.CheckProduct("ArcGISPro") == "Available":
-    raise RuntimeError("ArcGIS Pro许可不可用。")
-
+import pandas
 # %%
+a = arcpy.GetParameterAsText(0)
